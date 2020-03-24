f5dc04c7f6b8f30deeb078f98181cefcee17a0f0
Hyper_Shio <foruniversty@gmail.com>
2020-03-25 04:40:52 +0700

    adding pakage and refactor name

--- src/main/java/practice/java/springboot/web/auth/jwt/JwtApplication.java
+++ src/main/java/practice/java/springboot/web/auth/jwt/JwtApplication.java
@@ -1,13 +1,13 @@
-package practice.java.springboot.web.auth.authentication;
+package practice.java.springboot.web.auth.jwt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthenticationApplication {
+public class JwtApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticationApplication.class, args);
+		SpringApplication.run(JwtApplication.class, args);
 	}
 
 }
